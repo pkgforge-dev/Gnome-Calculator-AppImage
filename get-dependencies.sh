63b81560a32bd7174fd99e7654ba23790b421455
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-08-10 11:15:49 +0200

    Test

---
+++ get-dependencies.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+set -eux
+
+sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
+
+if [ "$(uname -m)" = 'x86_64' ]; then
+	PKG_TYPE='x86_64.pkg.tar.zst'
+else
+	PKG_TYPE='aarch64.pkg.tar.xz'
+fi
+
+MESA_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/mesa-mini-$PKG_TYPE"
+LIBXML_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/libxml2-iculess-$PKG_TYPE"
+LLVM_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/llvm-libs-nano-$PKG_TYPE"
+# opus is not used by the app, it's just to debloat the base install
+OPUS_URL="https://github.com/pkgforge-dev/llvm-libs-debloated/releases/download/continuous/opus-nano-$PKG_TYPE"
+
+echo "Installing build dependencies for sharun & AppImage integration..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	base-devel \
+	curl \
+	desktop-file-utils \
+	git \
+	libxtst \
+	wget \
+	xorg-server-xvfb \
+	zsync
+# TODO: Build the app from source instead of relying on Arch repos
+echo "Installing app & it's dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm \
+	gnome-calculator \
+        mesa
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$LLVM_URL"    -O  ./llvm-libs.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$LIBXML_URL"  -O  ./libxml2-iculess.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$OPUS_URL"    -O  ./opus-nano.pkg.tar.zst
+wget --retry-connrefused --tries=30 "$MESA_URL"    -O  ./mesa.pkg.tar.zst
+
+pacman -U --noconfirm ./*.pkg.tar.zst
+rm -f ./*.pkg.tar.zst
+
+echo "All done!"
+echo "---------------------------------------------------------------"
---
+++ gnome-calculator-appimage.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+PACKAGE=gnome-calculator
+ICON=/usr/share/icons/hicolor/scalable/apps/org.gnome.Calendar.svg
+DESKTOP=/usr/share/applications/org.gnome.Calculator.desktop
+URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+
+VERSION=$(pacman -Q "$PACKAGE" | awk 'NR==1 {print $2; exit}')
+[ -n "$VERSION" ] && echo "$VERSION" > ~/version
+
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME="$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
+
+# Prepare AppDir
+mkdir -p ./AppDir/shared/lib
+cd ./AppDir
+
+# Copy desktop file & icon
+cp -v "$DESKTOP"   ./
+cp -v "$ICON"      ./
+cp -v "$ICON"      ./.DirIcon
+
+# Patch StartupWMClass to work on X11
+# Doesn't work when ran in Wayland, as it's 'org.gnome.Calculator' instead.
+# It needs to be manually changed by the user in this case.
+sed -i '/^\[Desktop Entry\]/a\
+StartupWMClass=gnome-calculator
+' "$DESKTOP"
+
+# DEPLOY ALL LIBS
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+export DST_DIR="$PWD"/AppDir
+./quick-sharun /usr/bin/gnome-calculator
+
+# MAKE APPIMAGE WITH URUNTIME
+wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
+chmod +x ./uruntime2appimage
+./uruntime2appimage
+
+# Set up the PELF toolchain
+wget --retry-connrefused --tries=30 \
+	"https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH" -O ./pelf
+chmod +x ./pelf
+
+echo "Generating [dwfs]AppBundle...(Go runtime)"
+./pelf --add-appdir ./AppDir \
+	--appbundle-id="$PACKAGE-$VERSION" \
+	--compression "-C zstd:level=22 -S26 -B8" \
+	--output-to "$PACKAGE-$VERSION-anylinux-$ARCH.dwfs.AppBundle" \
+
+zsyncmake *.AppBundle -u *.AppBundle
+
+echo "All Done!"
