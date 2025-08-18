4fee71f8a8871e210069553c1b2a6a1d998af847
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-17 21:24:52 -0400

    typo

--- gnome-calculator-appimage.sh
+++ gnome-calculator-appimage.sh
@@ -27,7 +27,7 @@ cp -vr /usr/share/devhelp ./AppDir/share/
 
 # fix TLS issue
 # TODO remove this once quick-sharun adds this fix
-sed -i 's|/usr|././|g' ./AppDir/shared/lib/lib/libp11-kit* ./AppDir/shared/lib/pkcs11/*
+sed -i 's|/usr|././|g' ./AppDir/shared/lib/libp11-kit* ./AppDir/shared/lib/pkcs11/*
 cp -rv /usr/share/p11-kit         ./AppDir/share
 cp -rv /usr/share/ca-certificates ./AppDir/share
 
