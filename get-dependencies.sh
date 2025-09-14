5bd429664be9689f75636ac2c49be75bfe2093c2
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-09-14 12:28:36 +0200

    Update get-dependencies.sh for newer template

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -2,8 +2,6 @@
 
 set -eux
 
-sed -i 's/DownloadUser/#DownloadUser/g' /etc/pacman.conf
-
 DEBLOATED_PKGS_INSTALLER="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
 
 echo "Installing build dependencies for sharun & AppImage integration..."
@@ -28,5 +26,6 @@ wget --retry-connrefused --tries=30 "$DEBLOATED_PKGS_INSTALLER" -O ./get-debloat
 chmod +x ./get-debloated-pkgs.sh
 ./get-debloated-pkgs.sh libxml2-mini mesa-nano gtk4-mini
 
-echo "All done!"
+echo "Extracting the app version into a version file"
 echo "---------------------------------------------------------------"
+pacman -Q gnome-calculator | awk '{print $2; exit}' > ~/version
