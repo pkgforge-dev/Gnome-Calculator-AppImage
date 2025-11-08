9985f9d953f76694a97f9eee68e094377a890e6e
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-08 10:16:28 +0100

    Simplify the AppImage making script

--- gnome-calculator-appimage.sh
+++ gnome-calculator-appimage.sh
@@ -3,25 +3,21 @@
 set -eux
 
 ARCH="$(uname -m)"
-PACKAGE=gnome-calculator
-URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 VERSION="$(cat ~/version)"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
 # Variables used by quick-sharun
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export OUTNAME="$PACKAGE"-"$VERSION"-anylinux-"$ARCH".AppImage
+export OUTNAME=gnome-calculator-"$VERSION"-anylinux-"$ARCH".AppImage
 export DESKTOP=/usr/share/applications/org.gnome.Calculator.desktop
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.gnome.Calculator.svg
 export DEPLOY_OPENGL=1
 export STARTUPWMCLASS=gnome-calculator # For Wayland, this is 'org.gnome.Calculator', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
 
-# DEPLOY ALL LIBS
+# Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
-./quick-sharun /usr/bin/gnome-calculator /usr/bin/gcalccmd /usr/lib/gnome-calculator-search-provider
-cp -vr /usr/share/vala ./AppDir/share/
-cp -vr /usr/share/devhelp ./AppDir/share/
+./quick-sharun /usr/bin/gnome-calculator /usr/bin/gcalccmd /usr/lib/gnome-calculator-search-provider /usr/share/vala /usr/share/devhelp
 
 ## Copy help files for Help section to work
 langs=$(find /usr/share/help/*/gnome-calculator/ -type f | awk -F'/' '{print $5}' | sort | uniq)
@@ -30,24 +26,19 @@ for lang in $langs; do
   cp -vr /usr/share/help/$lang/gnome-calculator/* ./AppDir/share/help/$lang/gnome-calculator/
 done
 
-## Further debloat locale
-find ./AppDir/share/locale -type f ! -name '*glib*' ! -name '*gnome-calculator*' -delete
-
-## Set gsettings to save to keyfile, instead to dconf
-echo "GSETTINGS_BACKEND=keyfile" >> ./AppDir/.env
-
 ## Copy files needed for search integration
 mkdir -p ./AppDir/share/gnome-shell/search-providers/
 cp -v /usr/share/gnome-shell/search-providers/org.gnome.Calculator-search-provider.ini ./AppDir/share/gnome-shell/search-providers/org.gnome.Calculator-search-provider.ini
 mkdir -p ./AppDir/share/dbus-1/services/
 cp -v /usr/share/dbus-1/services/org.gnome.Calculator.SearchProvider.service ./AppDir/share/dbus-1/services/org.gnome.Calculator.SearchProvider.service
 
-# MAKE APPIMAGE WITH URUNTIME
-wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
-chmod +x ./uruntime2appimage
-./uruntime2appimage
+## Set gsettings to save to keyfile, instead to dconf
+echo "GSETTINGS_BACKEND=keyfile" >> ./AppDir/.env
 
-# PREPARE APPIMAGE FOR RELEASE
+# Make the AppImage with uruntime
+./quick-sharun --make-appimage
+
+# Prepare the AppImage for release
 mkdir -p ./dist
 mv -v ./*.AppImage* ./dist
 mv -v ~/version     ./dist
