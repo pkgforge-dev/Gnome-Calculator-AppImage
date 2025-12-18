e794c4c38611e1093faaa03f867531a9b0609a33
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-12-18 12:48:52 +0100

    Add `GTK_CLASS_FIX` for StartupWMClass fix on Wayland

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,7 +11,8 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.gnome.Calculator.svg
 export DESKTOP=/usr/share/applications/org.gnome.Calculator.desktop
 export DEPLOY_OPENGL=1
-export STARTUPWMCLASS=gnome-calculator # For Wayland, this is 'org.gnome.Calculator', so this needs to be changed in desktop file manually by the user in that case until some potential automatic fix exists for this
+export STARTUPWMCLASS=gnome-calculator
+export GTK_CLASS_FIX=1
 
 # Trace and deploy all files and directories needed for the application (including binaries, libraries and others)
 quick-sharun /usr/bin/gnome-calculator \
